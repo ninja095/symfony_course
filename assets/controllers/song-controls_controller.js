b2d0c0cc617312c9636cc3c1e0147c7ea3bada3b
ninja095 <ninja095@gmail.com>
2022-09-15 15:25:08 +0200

    added futures

--- assets/controllers/song-controls_controller.js
+++ assets/controllers/song-controls_controller.js
@@ -1,18 +1,24 @@
 import { Controller } from '@hotwired/stimulus';
-import axios from "axios";
 
 /*
  * This is an example Stimulus controller!
  *
  * Any element with a data-controller="hello" attribute will cause
  * this controller to be executed. The name "hello" comes from the filename:
- * song-controls_controller.js -> "hello"
+ * hello_controller.js -> "hello"
  *
  * Delete this file or adapt it for your use!
  */
+import axios from 'axios';
+
 export default class extends Controller {
+
   play(event) {
     event.preventDefault();
-    console.log('Playing!');
+
+    axios.get(this.infoUrlValue)
+        .then((response) => {
+          console.log(response);
+        });
   }
 }
--- src/Controller/SongController.php
+++ src/Controller/SongController.php
@@ -10,7 +10,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class SongController extends AbstractController
 {
-    #[Route('/api/songs/{id<\d+>}', methods: ['GET'])]
+    #[Route('/api/songs/{id<\d+>}', name: 'api_songs_get_one', methods: ['GET'])]
     public function getSong(int $id, LoggerInterface $logger): Response
     {
         // TODO query the database
